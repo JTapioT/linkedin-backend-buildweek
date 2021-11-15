b7d79f6708bef06b8be05e3f354cc4a050bf5237
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-11-15 09:50:21 +0100

    Initialize with files

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+const PORT = process.env.PORT || 3005;
+
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+server.listen(PORT, () => {
+console.table(listEndpoints(server));
+console.log("Server is running on port: ", PORT);
+ });
+});
+
