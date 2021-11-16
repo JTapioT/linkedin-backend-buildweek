5c9f67f0e1e6b40cdfc6234e18cb155da8bae77c
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-16 01:22:45 +0100

    added function for default image update

--- src/services/profile/schema.js
+++ src/services/profile/schema.js
@@ -1,19 +1,75 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import validation from "validator";
 
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
-const ProfileSchema = new Schema({
-    
-        name: {type: String, required: true},
-        surname: {type: String, required: true},
-        email: {type: String, required: true},
-        bio: {type: String, required: true},
-        title: {type: String, required: true},
-        area: {type: String, required: true},
-        image: {type: String, required: true, default: `https://i1.wp.com/suiteplugins.com/wp-content/uploads/2019/10/blank-avatar.jpg?fit=800%2C800&ssl=1`},
-},{
+const ProfileSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value) => {
+          if (!validation.isLength(value, { min: 2 })) {
+            throw new Error("Name should be at least 2 characters long!");
+          }
+        },
+      },
+    },
+    surname: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value) => {
+          if (!validation.isLength(value, { min: 2 })) {
+            throw new Error("Surname should be at least 2 characters long!");
+          }
+        },
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: async (value) => {
+          if (!validation.isEmail(value)) {
+            throw new Error("Email is invalid");
+          } else {
+            const checkEmail = await ProfileModel.findOne({ email: value });
+            if (checkEmail) {
+              throw new Error("Email already existsts");
+            }
+          }
+        },
+      },
+    },
+    bio: { type: String, required: true },
+    title: { type: String, required: true },
+    area: { type: String, required: true },
+    image: {
+      type: String,
+      required: true,
+      default: function () {
+        return `https://eu.ui-avatars.com/api/?name=${this.name}+${this.surname}`;
+      },
+    },
+    cover: {
+      type: String,
+    },
+  },
+  {
     timestamps: true,
-}
-)
+  }
+);
 
-export default model("Profile", ProfileSchema)
+ProfileSchema.pre("findOneAndUpdate", async function (next) {
+    const modelToUpdate = await this.model.findOne(this.getFilter())
+    if (modelToUpdate?.image.includes("eu.ui-avatars.com")) {
+      this.set({
+        image: `https://eu.ui-avatars.com/api/?name=${modelToUpdate.name}+${modelToUpdate.surname}`,
+      })
+    }
+    next()
+  })
+
+export default model("Profile", ProfileSchema);
