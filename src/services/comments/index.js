c71b5df562341bce29868bccb177f3cfcd971e3d
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-17 13:13:55 +0100

    Bug fixed for get post comments

--- src/services/comments/index.js
+++ src/services/comments/index.js
@@ -7,53 +7,43 @@ import PostSchema from "../posts/schema.js";
 const commentsRouter = express.Router();
 
 // **************** POST A NEW COMMENT ****************
-commentsRouter.post("/:postId/post", async (req, res, next) => {
-  try {
+commentsRouter.post("/:postId/comment", async (req, res, next) => {
+  {
     const postId = req.params.postId;
-    const post = await PostSchema.findById(postId);
-    if (post) {
-      const newComment = new CommentSchema(req.body);
-      newComment.postID = postId;
-      const { _id } = await newComment.save();
-      post.comments.push(_id);
-      await post.save();
-
-      res.status(201).send(newComment);
-    } else {
-      next(createError(404, `post with id ${postId} was not found`));
-    }
-  } catch (error) {
-    console.log(error);
-    if (error.name === "ValidationError") {
+    const newComment = new CommentSchema({ ...req.body, postId });
+    try {
+      const createdComment = await newComment.save();
+      res.status(201).json(createdComment);
+    } catch (error) {
       next(createError(400, error));
-    } else {
-      console.log(error);
-      next(createError(500, "An Error ocurred while creating your comment"));
     }
   }
 });
 
 // **************** GET ALL COMMENTS ****************
 commentsRouter.get("/:postId/comments", async (req, res, next) => {
-  try {
-    const postId = req.params.postId;
-    const comments = await PostSchema.findById(postId);
 
-    console.log(comments);
-    console.log(postId);
-    if (postId && comments) {
-      res.status(200).send(comments);
-    } else {
-      next(createError(404, `comments for post - ${postId} - cannot be found`));
-    }
+  try {
+    const comments = await CommentSchema.find({ postId: req.params.postId })
+    res.json(comments)
   } catch (error) {
-    if (error.name === "validationError") {
-      next(createError(400, error));
-    } else {
-      console.log(error);
-      next(createError(500, "An Error ocurred"));
-    }
+    next(createError(500, error))
   }
+
+  // try {
+  //   const postId = req.params.postId;
+  //   const comments = await PostSchema.findById(postId);
+
+  //   console.log(comments);
+  //   console.log(postId);
+  //   if (postId && comments) {
+  //     res.status(200).send(comments);
+  //   } else {
+  //     next(createError(404, `comments for post - ${postId} - cannot be found`));
+  //   }
+  // } catch (err) {
+  //   next(createError(500, "Error occurred while getting all the post"));
+  // }
 });
 
 // **************** GET SINGLE COMMENT ****************
@@ -64,18 +54,14 @@ commentsRouter.get("/:commentId", async (req, res, next) => {
     if (comment) {
       res.status(200).send(comment);
     } else {
-      next(createError(404, `comment with id ${commentId} was not found`));
+      next(createError(404, `Comment with id ${commentId} was not found`));
     }
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      next(createError(400, error));
-    } else {
-      console.log(error);
-      next(createError(500, "An Error ocurred while creating your comment"));
-    }
+  } catch (err) {
+    next(createError(500, "Error occurred while getting a single post"));
   }
 });
 
+// *************** EDIT A COMMENT ***************
 commentsRouter.put("/:commentId/edit", async (req, res, next) => {
   try {
     const commentId = req.params.commentId;
@@ -93,15 +79,27 @@ commentsRouter.put("/:commentId/edit", async (req, res, next) => {
     } else {
       next(createError(404, `post with id ${postId} was not found`));
     }
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      next(createError(400, error));
-    } else {
-      console.log(error);
-      next(createError(500, "An Error ocurred while editing your comment"));
-    }
+  } catch (err) {
+    next(createError(500, "Error occurred while updating the post"));
   }
 });
 
+// ******************* DELETE COMMENTS *******************
+commentsRouter.delete("/:postId/:commentId/delete", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    const post = await PostModel.findById(postId);
+    const comment = await CommentModel.findById(commentId);
+    if (post && comment) {
+      await CommentModel.findByIdAndDelete(commentId);
+      res.status(204).send(`deleted`);
+    } else {
+      next(createError(404, `An Error ocurred while deleting your comment`));
+    }
+  } catch (err) {
+    next(createError(500, "Error occurred while deleting the post"));
+  }
+});
 
 export default commentsRouter;
